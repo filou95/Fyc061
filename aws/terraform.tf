7b9a560265906bb50af109c938080caca757f7f6
filou95 <44317060+filou95@users.noreply.github.com>
2021-02-14 15:39:38 +0100

    Update terraform.tf

--- aws/terraform.tf
+++ aws/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         encrypt = true
-        bucket  = "projet-annuel"
+        bucket  = "fyc"
         region  = "eu-west-3"
         key     = "projet.tfstate"
     }
